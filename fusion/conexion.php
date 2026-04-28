c6d178b009de40dedd2ca5ab7feff9dd399ceb21
aguscecchsagrado <agustinacecch@sagradocorazon.edu.ar>
2026-04-28 18:04:28 -0300

    Elimino fusión

--- fusion/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "usuarios";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
--- fusion/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <p>Trackify © 2026</p>
-</footer>
-
-<script src="js/main.js?v=3"></script>
-<?php if(isset($extra_js)) echo $extra_js; ?>
-
-</body>
-</html>
